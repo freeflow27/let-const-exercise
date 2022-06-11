84775a225c6d976132a379f165102f29dda742e5
Alvaro Davison <adavison813@gmail.com>
2022-06-11 12:27:08 -0400

    let/const exercise

---
+++ app.js
@@ -0,0 +1,10 @@
+const PI = 3.14
+PI = 42 //wont work becasue const
+
+//var is loose, and can be redeclared and ressigned. Let can be reassigned, but not redeclared
+
+//var is loose, and can be redeclared and reassigned. Const cannot to eather. Once it is set, it stays that way.
+
+//let may be reassigned, while const stays the same. const is unmovable once it is defined.
+
+//setting a variable to be undefined and running it before the code that defines it. Like having the code at the top, but not defining it until later in the script.
